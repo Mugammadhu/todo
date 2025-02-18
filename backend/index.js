b245c4197a1f47befd7c75cdee00da000b4f8921
Mugammadhu <mugammadhuoff@gmail.com>
2025-02-18 20:28:18 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,15 @@ connectDatabase();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+// const corsOptions = {
+//   origin: 'https://todo-client-alpha-wheat.vercel.app', // Only allow requests from your frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Authorization', 'Content-Type'], // Allow Authorization and Content-Type headers
+//   credentials: true, // Allow cookies and other credentials
+// };
 
 const corsOptions = {
-  origin: 'https://todo-client-alpha-wheat.vercel.app', // Only allow requests from your frontend domain
+  origin: process.env.APPLICATION_URL, // Only allow requests from your frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Authorization', 'Content-Type'], // Allow Authorization and Content-Type headers
   credentials: true, // Allow cookies and other credentials
