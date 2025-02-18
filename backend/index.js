8009294d9773fc8e7cce5a3e5745da7a7d683168
Mugammadhu <mugammadhuoff@gmail.com>
2025-02-18 17:42:12 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ connectDatabase();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+// app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+
 
 
 
